7028e92cfbe918cdc840de8e258495a1d9a6f93a
Roman Tretyak <roma_tretyak@ukr.net>
2017-09-12 20:15:14 +0300

    js

--- app/js/main.js
+++ app/js/main.js
@@ -12,11 +12,11 @@ function createTodoItem(){
 	editInput.className = 'textfield';
 
 	const editButton = document.createElement('button');
-	editButton.innerText = 'Изменить';                 //присвоение внутреннего текста
+	editButton.innerText = 'Edit';                 //присвоение внутреннего текста
 	editButton.className = 'edit';
 
 	const deleteButton = document.createElement('button');
-	deleteButton.innerText = 'Удалить';
+	deleteButton.innerText = 'Delete';
 	deleteButton.className = 'delete';
 
 	const listItem = document.createElement('li');
@@ -30,9 +30,22 @@ function createTodoItem(){
 	listItem.appendChild(editButton);
 	listItem.appendChild(deleteButton);
 
+	bindEvents(listItem);    //подписка на событие
+
 	return listItem; 
 
 		                                                
+}
+// Функция привязки событий
+function bindEvents(todoItem) {
+	const checkbox = todoItem.querySelector('.checkbox');
+	const editButton = todoItem.querySelector('button.edit');
+	const deleteButton = todoItem.querySelector('button.delete');
+
+		//Запись на событие
+	checkbox.addEventListener('change', toggleTodoItem);
+	editButton.addEventListener('click', editTodoItem);
+	deleteButton.addEventListener('click', deleteTodoItem);
 }
 
 
@@ -42,11 +55,41 @@ function addTodoItem(event){
 
 	if(addInput.value === '') return alert('Введите название задачи');
 
-	const listItem = createTodoItem(addInput.value);                 /*ввод пользователь в качестве задачи */
+	const todoItem = createTodoItem(addInput.value);                 /*ввод пользователь в качестве задачи */
 	todoList.appendChild(todoItem);                                  /*добавление DOM-елемента в список */
-	addInput.value = '';
+	addInput.value = '';                                             /*Пустая строка после добавления задачи */ 
 }
 
+
+function toggleTodoItem(){
+	const listItem = this.parentNode; // доступ к родительскому елементу listItem
+	listItem.classList.toggle('completed');
+
+
+}
+
+function editTodoItem(){
+	const listItem = this.parentNode;
+	const title = listItem.querySelector('.title');
+	const editInput = listItem.querySelector('.textfield');
+	const isEditing = listItem.classList.contains('editing'); /* есть ли у листа класс editing*/
+
+	if(isEditing){
+		title.innerText = editInput.value; // присваеваем текст из поля для редактирования в поле для задач
+		this.innerText = 'Edit';
+	} else{
+		editInput.value = title.innerText;
+		this.innerText = 'Save';
+	}
+
+	listItem.classList.toggle('editing');    // убираем класс 
+
+}
+function deleteTodoItem(){
+	const listItem = this.parentNode;
+	todoList.removeChild(listItem);
+
+}
 
 const todoForm = document.getElementsByClassName('todo-form');         /* Доступ к форме*/
 const addInput = document.getElementsByClassName('todo-input');        /* Доступ к полю*/
@@ -54,7 +97,9 @@ const todoList = document.getElementsByClassName('todo-list');         /* Дос
 const todoItem = document.querySelectorAll('todo-item');             /* Доступ к задаче*/
 const listItem = createTodoItem(addInput.value); 
 
+function main(){
+	todoForm.addEventListener('submit', addTodoItem);                   /*привязка обработчика событий на submit*/ 
+	todoItems.forEach(item => bindEvents(item));
+}
 
-
-
-todoForm.addEventListener('submit', addTodoItem);                   /*привязка обработчика событий на submit*/
+main();
